ee408980aae7b5ab77cbe710d1bbb80ed0dc8221
KarlTred265 Tred <tred26559@gmail.com>
2023-07-24 16:08:47 +0000

    add new host name for herkou

--- trip_api/settings.py
+++ trip_api/settings.py
@@ -59,8 +59,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['trip-api-dc90bf17ef25.herokuapp.com']
-
+ALLOWED_HOSTS = ['localhost', 'trip-api.herokuapp.com']
 
 # Application definition
 
